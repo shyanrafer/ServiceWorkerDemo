bf2ed6715fe1a9ff79cdc294a256f92977e3fbd0
The Goose <ryanshaffer1324@gmail.com>
2024-09-25 11:48:49 -0400

    Forked starter repo, added some code from week 19 mini project, slight edits to readme, added comments throughout project to explain thoughts and give credit.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,11 +1,15 @@
+// all but plugins and module is starter code - plugins and module was copied from mini project week 19
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// under plugins
 // TODO: Add CSS loaders and babel to webpack.
+// under module
 
+// note to self - files will need to be changed in the following code to ensure proper function
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +22,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      // Webpack plugin that generates our html file and injects our bundles. 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
+     
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
 
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Never forget your contacts!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Text Editor Starter Code
+# ServiceWorkerDemo
+
+## Credit 
+Starter code provided from https://github.com/coding-boot-camp/cautious-meme
+Main package.json file was inspired by the week 19 mini project. 
