17b7b90460be5219685e87fe0cd1f7acb055d431
The Goose <ryanshaffer1324@gmail.com>
2024-09-25 12:35:43 -0400

    added gitignore, installed dependencies, edited database, install, src-sw, and readme.

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,12 +1,17 @@
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+// precache
 precacheAndRoute(self.__WB_MANIFEST);
-
+// setup fallback
+offlineFallback({
+  pageFallback: '/index.html', // Specify the fallback page
+});
+// setup cache
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -27,4 +32,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// copied from week 19
+registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'page-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
